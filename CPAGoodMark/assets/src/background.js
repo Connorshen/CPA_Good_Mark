5aa656abae3479e29cd12de68a1efc139aeefea7
比尔丶盖子 <914138410@qq.com>
2020-02-06 19:37:29 +0800

    add

--- CPAGoodMark/assets/src/background.js
+++ CPAGoodMark/assets/src/background.js
@@ -19,16 +19,19 @@ cc.Class({
         far_speed: 0,
     },
 
-    // use this for initialization
     onLoad: function () {
-
+        this.fixBgPos(this.far_bg[0], this.far_bg[1]);
+        //this.fixBgPos(this.near_bg[0], this.near_bg[1]);
     },
 
-
+    fixBgPos: function (bg1, bg2) {
+        bg1.x = 0;
+        var bg1BoundingBox = bg1.getBoundingBox();
+        bg2.setPosition(bg1BoundingBox.xMax, bg1BoundingBox.yMin);
+    },
     update: function (dt) {
-        //this.bgMove(this.far_bg, this.far_speed);
+        this.bgMove(this.far_bg, this.far_speed);
         //this.bgMove(this.near_bg, this.near_speed);
-        cc.log(this.far_bg[0].x);
     },
     bgMove: function (bgList, speed) {
         for (var index = 0; index < bgList.length; index++) {
